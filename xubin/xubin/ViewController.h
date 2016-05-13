a7b9cb3fa7395a691b1b82b1fad79e9a2868a637
xubin <123495351@qq.com>
2016-05-13 23:42:51 +0000

    #task  new

---
+++ xubin/xubin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xubin
+//
+//  Created by xubin on 16/5/13.
+//  Copyright © 2016年 xubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xubin/xubin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xubin
+//
+//  Created by xubin on 16/5/13.
+//  Copyright © 2016年 xubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
